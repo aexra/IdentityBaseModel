6e4441b1836569dff7de4263cb23501775ad75ff
Nikita Fomin <defdXs@yandex.ru>
2024-12-11 01:56:02 +0300

    No more useless methods

--- IdentityBase/Data/Contexts/IdentityContext.cs
+++ IdentityBase/Data/Contexts/IdentityContext.cs
@@ -1,27 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityBase.Data.Contexts;
 public class IdentityContext<T> : IdentityDbContext<T> where T : IdentityUser
 {
     public DbSet<T> Users { get; set; }
-    public async Task SeedDataAsync(IServiceProvider serviceProvider)
-    {
-        string[] roles = ["User", "Admin"];
-
-        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-        // ENSURE ROLES
-        foreach (var roleName in roles)
-        {
-            if (!roleManager.Roles.ToList().Exists(r => r.Name == roleName))
-            {
-                await roleManager.CreateAsync(new IdentityRole { Name = roleName });
-            }
-        }
-
-        SaveChanges();
-    }
 }
